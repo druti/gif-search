da68baefcf066ac23d90bd1b4ff597b98e264717
Andres Carbo <andres@depnes.com>
2017-09-13 19:07:14 -0400

    Added pseudo loader to images.

--- src/Components/Gif.js
+++ src/Components/Gif.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Gif = props => (
   <li className="gif-wrap">
-    <img src={props.url} role='presentation' />
+    <img src={props.url} role="presentation" />
   </li>
 );
 
